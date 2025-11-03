73fff3e231f30d1f2d4dd15ca7d86e914af7bf90
scarr33 <suchakb33@gmail.com>
2025-11-04 01:44:25 +0530

    evil commit

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,9 +1,18 @@
 interface ButtonProps {
-  onClick: () => void;
+  onClick?: () => void;
 }
 
 export const Button = (props: ButtonProps) => {
   const { onClick } = props;
+  const handler = () => {
+    console.log("hey");
+  };
+  if (!onClick)
+    return (
+      <button onClick={onClick} data-testid="button-test">
+        button
+      </button>
+    );
   return (
     <button onClick={onClick} data-testid="button-test">
       button
