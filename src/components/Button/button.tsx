89433803061d8be212deef9b4923cb338bc60e72
scarr33 <suchakb33@gmail.com>
2025-11-06 11:57:14 +0530

    dummy commit

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -4,6 +4,7 @@ interface ButtonProps {
 
 export const Button = (props: ButtonProps) => {
   const { onClick } = props;
+
   return (
     <button onClick={onClick} data-testid="button-test">
       button
